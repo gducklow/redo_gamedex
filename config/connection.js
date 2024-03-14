17b8198306a01cecf90b1dcf989f60495a6c119d
gducklow <gducklow@gmail.com>
2024-03-14 16:37:22 -0500

    added and removed bcrypt

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.POSTGRES_DB) {
   sequelize = new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
-      process.env.DB_PASSWORD,
+      process.env.DB_PASS,
       {
         host: 'localhost',
         dialect: 'mysql',
